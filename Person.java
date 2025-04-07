375a8f9eb3cee5955461d769c74caa8c1a672fef
renski723 <laurensbiscocho17@gmail.com>
2025-04-07 17:59:56 +0800

    add preson file

--- Person.java
+++ Person.java
@@ -20,69 +20,11 @@ public class Person{
         System.out.println("ID: " + this.id);
     }
 }
- 
-class Student extends Person {
-    private String gradeLevel;
- 
-    public Student(String name, int id, String gradeLevel) {
-        super(name, id);
-        this.gradeLevel = gradeLevel;
-    }
- 
-    public String getGradeLevel() {
-        return this.gradeLevel;
-    }
- 
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Grade Level: " + this.gradeLevel);
-    }
-}
- 
-class Teacher extends Person {
-    private String subject;
- 
-    public Teacher(String name, int id, String subject) {
-        super(name, id);
-        this.subject = subject;
-    }
- 
-    public String getSubject() {
-        return this.subject;
-    }
- 
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Subject: " + this.subject);
-    }
-}
- 
-class Staff extends Person {
-    private String department;
- 
-    public Staff(String name, int id, String department) {
-        super(name, id);
-        this.department = department;
-    }
- 
-    public String getDepartment() {
-        return this.department;
-    }
- 
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Department: " + this.department);
-    }
-}
- 
-class SchoolTest {
+ class SchoolTest {
     public static void main(String[] args) {
         Student student = new Student("Renski", 1234, "College");
         Teacher teacher = new Teacher("Sir Archie", 2345, "OOP");
-        Staff staff = new Staff("Sir Joel", 3456, "Hardware");
+        Staff staff = new Staff("Sir Aramil", 3456, "Program Chair");
  
         System.out.println("  Students Information  ");
         student.displayInfo();
